0e26fa706f762a0917ddbf8134f15c01141cf560
sableaks <ghostyhax3r@gmail.com>
2026-09-13 20:29:44 +0300

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
+  // This securely checks the input against your Vercel environment variable
   if (req.body.password === process.env.SITE_PASSWORD) {
     return res.status(200).json({ success: true });
   } else {
